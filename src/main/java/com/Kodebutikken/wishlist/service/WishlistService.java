e249b93e5ebf414b6d91684746030611e48c6f16
Nikolaj Esberg <nikolajesberg@gmail.com>
2026-04-15 17:51:50 +0200

    wishlistService

--- src/main/java/com/Kodebutikken/wishlist/service/WishlistService.java
+++ src/main/java/com/Kodebutikken/wishlist/service/WishlistService.java
@@ -1,4 +1,45 @@
 package com.Kodebutikken.wishlist.service;
 
+
+import com.Kodebutikken.wishlist.model.Wishlist;
+import com.Kodebutikken.wishlist.repository.WishlistRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class WishlistService {
+    private final WishlistRepository wishlistRepository;
+
+    public WishlistService(WishlistRepository wishlistRepository) {
+        this.wishlistRepository = wishlistRepository;
+    }
+
+    public List<Wishlist> getWishlistByProfileId(Long profileId) {
+        return wishlistRepository.getWishlistsByProfileId(profileId);
+    }
+
+    public Wishlist getWishlistById(Long id) {
+        return wishlistRepository.getWishlistById(id);
+    }
+
+    public void createWishlist(Wishlist wishlist, Long profileId) {
+        wishlistRepository.createWishlist(wishlist, profileId);
+    }
+
+    public void deleteWishlist(Long id) {
+        wishlistRepository.deleteWishlist(id);
+    }
+
+    public void updateWishlist(Wishlist wishlist) {
+        wishlistRepository.updateWishlist(wishlist);
+    }
+
+    public void addProductToWishlist(Long wishlistId, Long productId) {
+        wishlistRepository.addProductToWishlist(wishlistId, productId);
+    }
+
+    public void removeProductFromWishlist(Long wishlistId, Long productId) {
+        wishlistRepository.removeProductFromWishlist(wishlistId, productId);
+    }
 }
